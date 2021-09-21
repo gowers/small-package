cf53a1453c09a15afbb2c68fe9d777af5c5affad
kenzok8 <kenzok8@gmail.com>
2021-09-22 01:17:40 +0800

    int

---
+++ diy/Modify.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# --------------------------------------------------------
+# Script for creating ACL file for each LuCI APP
+
+sed -i \
+-e "s/\(PKG_HASH\|PKG_MD5SUM\|PKG_MIRROR_HASH\):=.*/\1:=skip/" \
+-e 's?\.\./\.\./luci.mk?$(TOPDIR)/feeds/luci/luci.mk?' \
+-e 's/ca-certificates/ca-bundle/' \
+*/Makefile
+
+exit 0
