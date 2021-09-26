c53885367c999ceaab927e1b797e11777b3f9094
有種 <39034242+kenzok8@users.noreply.github.com>
2021-09-26 17:57:40 +0800

    Update Modify.sh

--- diy/Modify.sh
+++ diy/Modify.sh
@@ -6,8 +6,13 @@ sed -i \
 -e "s/\(PKG_HASH\|PKG_MD5SUM\|PKG_MIRROR_HASH\):=.*/\1:=skip/" \
 -e 's?2. Clash For OpenWRT?3. Applications?' \
 -e 's?\.\./\.\./luci.mk?$(TOPDIR)/feeds/luci/luci.mk?' \
--e "s/PKG_RELEASE:=.*/PKG_RELEASE:=$(AUTORELEASE)/"  \
 -e 's/ca-certificates/ca-bundle/' \
 */Makefile
+
+for pkg in $(ls -d */); do
+if [ "$(grep "PKG_RELEASE" $pkg/Makefile)" ]; then
+sed -i "s/PKG_RELEASE:=.*/PKG_RELEASE:=$(git rev-list --count master $pkg)/" $pkg/Makefile || true
+fi
+done
 
 exit 0
